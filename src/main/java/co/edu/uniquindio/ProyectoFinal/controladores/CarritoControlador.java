b16b0344062e9c47217b10c89649ceaf52551475
carlos.perez <carlospverano@gmail.com>
2024-10-10 14:31:23 -0500

    correcion OrdenControlador

--- src/main/java/co/edu/uniquindio/ProyectoFinal/controladores/CarritoControlador.java
+++ src/main/java/co/edu/uniquindio/ProyectoFinal/controladores/CarritoControlador.java
@@ -20,36 +20,36 @@ public class CarritoControlador {
     @PostMapping("/crear-carrito/{idUsuario}")
     public ResponseEntity<MensajeDTO<String>> crearCarrito(@PathVariable String idUsuario) throws Exception {
         carritoServicio.crearCarrito(idUsuario);
-        return ResponseEntity.ok().body(new MensajeDTO<String>(false, "Se ha creado el carrito correctamente"));
+        return ResponseEntity.ok().body(new MensajeDTO<>(false, "Se ha creado el carrito correctamente"));
     }
 
     @PostMapping("/agregar-evento")
     public ResponseEntity<MensajeDTO<String>> agregarEvento(@RequestBody DetalleCarritoDTO detalle) throws Exception {
         carritoServicio.agregarEventoCarrito(detalle);
-        return ResponseEntity.ok().body(new MensajeDTO<String>(false, "Se ha agregado el evento al carrito correctamente"));
+        return ResponseEntity.ok().body(new MensajeDTO<>(false, "Se ha agregado el evento al carrito correctamente"));
     }
 
     @DeleteMapping("/eliminar-item/")
     public ResponseEntity<MensajeDTO<String>> eliminarEventoCarrito(@RequestBody EliminarEventoCarritoDTO  eliminarEventoCarritoDTO) throws Exception{
         carritoServicio.eliminarEventoCarrito(eliminarEventoCarritoDTO);
-        return ResponseEntity.ok().body(new MensajeDTO<String>(false, "Se ha eliminado el evento del carrito correctamente"));
+        return ResponseEntity.ok().body(new MensajeDTO<>(false, "Se ha eliminado el evento del carrito correctamente"));
     }
 
     @DeleteMapping("/vaciar-carrito/{idCarrito}")
     public ResponseEntity<MensajeDTO<String>> vaciarCarrito(@PathVariable String idCarrito) throws Exception {
         carritoServicio.limpiarCarrito(idCarrito);
-        return ResponseEntity.ok().body(new MensajeDTO<String>(false, "Se ha vaciado el carrito correctamente"));
+        return ResponseEntity.ok().body(new MensajeDTO<>(false, "Se ha vaciado el carrito correctamente"));
     }
 
     @GetMapping("/obtener-info-carrito/{idCarrito}")
     public ResponseEntity<MensajeDTO<CarritoDTO>> obtenerCarrito(@PathVariable String idCarrito) throws Exception {
-        return  ResponseEntity.ok().body(new MensajeDTO<CarritoDTO>(false, carritoServicio.obtenerInformacionCarrito(idCarrito)));
+        return  ResponseEntity.ok().body(new MensajeDTO<>(false, carritoServicio.obtenerInformacionCarrito(idCarrito)));
     }
 
     @PutMapping("/actualizar-carrito/{idCarrito}")
     public ResponseEntity<MensajeDTO<String>> actualizarCarrito(@RequestBody ActualizarEventoCarritoDTO actualizarEventoCarritoDTO) throws Exception {
         carritoServicio.actualizarEventoCarrito(actualizarEventoCarritoDTO);
-        return ResponseEntity.ok().body(new MensajeDTO<String>(false, "Se ha actualizado el carrito correctamente"));
+        return ResponseEntity.ok().body(new MensajeDTO<>(false, "Se ha actualizado el carrito correctamente"));
     }
 
 }
--- src/main/java/co/edu/uniquindio/ProyectoFinal/controladores/OrdenControlador.java
+++ src/main/java/co/edu/uniquindio/ProyectoFinal/controladores/OrdenControlador.java
@@ -2,12 +2,14 @@ package co.edu.uniquindio.ProyectoFinal.controladores;
 
 import co.edu.uniquindio.ProyectoFinal.dto.jws.MensajeDTO;
 import co.edu.uniquindio.ProyectoFinal.dto.orden.CrearOrdenDTO;
+import co.edu.uniquindio.ProyectoFinal.dto.orden.InformacionOrdenDTO;
 import co.edu.uniquindio.ProyectoFinal.services.interfaces.OrdenServicio;
 import com.mercadopago.resources.preference.Preference;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -18,8 +20,9 @@ public class OrdenControlador {
     private final OrdenServicio ordenServicio;
 
     @PostMapping("/crear-orden")
-    public void crearOrden(CrearOrdenDTO crearOrdenDTO) throws Exception {
+    public ResponseEntity<MensajeDTO<String>> crearOrden(CrearOrdenDTO crearOrdenDTO) throws Exception {
         ordenServicio.crearOrden(crearOrdenDTO);
+        return ResponseEntity.ok().body(new MensajeDTO<>(false, "Orden creada correctamente"));
     }
 
     @PostMapping("/realizar-pago")
@@ -33,12 +36,12 @@ public class OrdenControlador {
     }
 
     @GetMapping("/historial-ordenes/{idUsuario}")
-    public ResponseEntity<MensajeDTO> historialOrdenes(@PathVariable String idUsuario) throws Exception{
-        return ResponseEntity.ok().body(new MensajeDTO<>(false, ordenServicio.obtenerInformacionOrden(idUsuario)));
+    public ResponseEntity<MensajeDTO<List<InformacionOrdenDTO>>> historialOrdenes(@PathVariable String idUsuario) throws Exception{
+        return ResponseEntity.ok().body(new MensajeDTO<>(false, ordenServicio.obtenerOrdenes(idUsuario)));
     }
 
     @GetMapping("/detalle-orden/{idOrden}")
-    public ResponseEntity<MensajeDTO> detalleOrden(@PathVariable String idOrden) throws Exception{
+    public ResponseEntity<MensajeDTO<InformacionOrdenDTO>> detalleOrden(@PathVariable String idOrden) throws Exception{
         return ResponseEntity.ok().body(new MensajeDTO<>(false, ordenServicio.obtenerInformacionOrden(idOrden)));
     }
 
